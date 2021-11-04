0d1ee633ea7dbf03cc11b0ea10ac329884750d59
adam <adam.szczepanski@serwus.pl>
2021-11-04 18:30:12 +0100

    Floating button jest i działa

--- js/about.js
+++ js/about.js
@@ -1,11 +1,3 @@
-jQuery(function($)	{
-        //scroll reset
-        $.scrollTo(0);
-
-        $('.floatButton').click(function() { $.scrollTo($('body'), 500); return false;});
-    }
-);
-
 $(window).scroll(function()	{
         if($(this).scrollTop() > 100) {
             $('.floatButton').fadeIn();
