e104d110341c0e7d7e23a34046a863a748ca86e5
adam <adam.szczepanski@serwus.pl>
2023-03-18 14:01:43 +0100

    Początek eksperymentów z animacjami

--- js/index.js
+++ js/index.js
@@ -4,7 +4,20 @@ const images = ['images/amabile-slide-1.jpg',
                 'images/amabile-slide-3.jpg',
                 'images/amabile-slide-4.jpg'];
 let index = 0;
-setInterval(nextSlide, 5000);
+
+const slajd = new Promise((end, error) => {
+    sliderBox.style.animation = 'slide-left 2s';
+    end();
+    error();
+    });
+slajd.then(
+    function end() {
+        console.log("Już!");
+    },
+    function error()    {
+        console.log("Oooops");
+    });
+/* setInterval(nextSlide, 5000); */
 
 function nextSlide()    {
     if (++index === images.length)    {
