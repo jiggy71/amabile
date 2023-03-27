aea077fb44c6fb2b6a59da11e05c81e0a0886996
Adam Sz <adam.szczepanski@serwus.pl>
2023-03-27 18:21:28 +0200

    Dodałem preloading obrazków

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
-const sliderBox = document.getElementById('slide-box');
 const images = ['images/amabile-slide-1.jpg',
                 'images/amabile-slide-2.jpg',
                 'images/amabile-slide-3.jpg',
                 'images/amabile-slide-4.jpg'];
-let index = 0;
+images.map(url => {
+    preloadImage(url);
+});
 
+let index = 0;
 setInterval(nextSlide, 5000);
 
 function nextSlide()    {
+    const sliderBox = document.getElementById('slide-box');
     sliderBox.classList.remove('fade-in-animation');
     let handler;
     sliderBox.classList.add('fade-out-animation');
@@ -20,4 +23,9 @@ function nextSlide()    {
         sliderBox.src = images[index];
         sliderBox.classList.add('fade-in-animation');
     });
+}
+
+function preloadImage(url)  {
+    const img = new Image();
+    img.src = url;
 }
